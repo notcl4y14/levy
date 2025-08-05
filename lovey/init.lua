b4afad0cc41b6269bb52f104073416eae1e5910e
notcl4y14 <notcl4y@gmail.com>
2025-08-05 13:15:38 +0300

    Add library-related info in the module

--- lovey/init.lua
+++ lovey/init.lua
@@ -4,7 +4,11 @@ local PATH = (...):gsub('%.[^%.]+$', '')
 -- LOVEY
 -- ================
 
-local lovey = {}
+local lovey = {
+	_URL = "https://github.com/notcl4y14/lovey",
+	_DESCRIPTION = "A Bevy-like ECS library for LOVE2D",
+	_VERSION = "1.0.0",
+}
 
 lovey.App       = require(PATH..".app")
 lovey.Entity    = require(PATH..".entity")
